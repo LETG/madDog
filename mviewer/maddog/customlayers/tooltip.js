5be617814785cf992620430a520743fb1dc39944
gaetanbrl <gaetan.bruel@jdev.fr>
2022-05-06 18:46:53 +0200

    add tooltip for commune #22

---
+++ mviewer/maddog/customlayers/tooltip.js
@@ -0,0 +1,24 @@
+
+let style = [new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(0,0,0,0.0)'
+    }),
+    stroke: new ol.style.Stroke({
+        color: 'rgba(0,0,0,0.0)',
+        width: 1
+    })
+})];
+
+let legend = { items: [] };
+
+let layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        format: new ol.format.GeoJSON(),
+        url: "https://gis.jdev.fr/geoserver/maddog/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=maddog%3Acommunewithsite&outputFormat=application%2Fjson"
+    }),
+    style: function(feature, resolution) {
+        return style;
+    }
+});
+
+new CustomLayer("tooltip", layer, legend);

0a79df46d00ad7105f7ee07ac16c2d6eddc942f4
gaetanbrl <gaetan.bruel@jdev.fr>
2022-05-12 11:46:34 +0200

    dynamic legend

--- mviewer/maddog/addons/maddog/js/utils/tdcUtils.js
+++ mviewer/maddog/addons/maddog/js/utils/tdcUtils.js
@@ -27,15 +27,22 @@ const tdcUtils = (function() {
                 // récupération du TDC
                 .then(tdc => {
                     maddog.charts.tdc = {
-                        ...tdc.data, features: tdc.data.features.map(f =>
-                            ({ ...f, properties: { ...f.properties, color: "#" + Math.floor(Math.random() * 16777215).toString(16) } })
+                        ...tdc.data,
+                        features: tdc.data.features.map(f =>
+                            ({
+                                ...f,
+                                properties: {
+                                    ...f.properties,
+                                    color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+                                }
+                            })
                         )
                     };
-                    // Affichage des TDC sur la carte
-                    tdcUtils.drawTDC(maddog.charts.tdc);
                     // Affichage du multi select avec les dates des TDC
                     tdcUtils.setTdcFeatures(tdc.data.features)
                     tdcUtils.createTDCMultiSelect();
+                    // Affichage des TDC sur la carte
+                    tdcUtils.changeTdc()
                 })
         },
         drawRefLine: () => {
@@ -82,6 +89,7 @@ const tdcUtils = (function() {
             });
             layerTdc.getSource().clear();
             layerTdc.getSource().addFeatures(featuresTdc);
+            
         },
         getRadiales: (r) => {
             // on affiche la radiale sur la carte
@@ -143,7 +151,8 @@ const tdcUtils = (function() {
                 line.y = labels.map(() => 0);
                 line.line = {
                     ...line.line,
-                    dash: 'dashdot', width: 4
+                    dash: 'dashdot',
+                    width: 4
                 };
             } else {
                 line.y = labels.map((radialeName, i) => {
@@ -218,24 +227,26 @@ const tdcUtils = (function() {
                         height: 600,
                         path: "M384 128h-128V0L384 128zM256 160H384v304c0 26.51-21.49 48-48 48h-288C21.49 512 0 490.5 0 464v-416C0 21.49 21.49 0 48 0H224l.0039 128C224 145.7 238.3 160 256 160zM255 295L216 334.1V232c0-13.25-10.75-24-24-24S168 218.8 168 232v102.1L128.1 295C124.3 290.3 118.2 288 112 288S99.72 290.3 95.03 295c-9.375 9.375-9.375 24.56 0 33.94l80 80c9.375 9.375 24.56 9.375 33.94 0l80-80c9.375-9.375 9.375-24.56 0-33.94S264.4 285.7 255 295z"
                     },
-                    click: function (gd) {
-                        Plotly.downloadImage(gd, { format: 'svg' })
+                    click: function(gd) {
+                        Plotly.downloadImage(gd, {
+                            format: 'svg'
+                        })
                     }
-                },{
+                }, {
                     name: 'Export CSV',
                     icon: {
                         width: 500,
                         height: 600,
                         path: "M224 0V128C224 145.7 238.3 160 256 160H384V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V64C0 28.65 28.65 0 64 0H224zM80 224C57.91 224 40 241.9 40 264V344C40 366.1 57.91 384 80 384H96C118.1 384 136 366.1 136 344V336C136 327.2 128.8 320 120 320C111.2 320 104 327.2 104 336V344C104 348.4 100.4 352 96 352H80C75.58 352 72 348.4 72 344V264C72 259.6 75.58 256 80 256H96C100.4 256 104 259.6 104 264V272C104 280.8 111.2 288 120 288C128.8 288 136 280.8 136 272V264C136 241.9 118.1 224 96 224H80zM175.4 310.6L200.8 325.1C205.2 327.7 208 332.5 208 337.6C208 345.6 201.6 352 193.6 352H168C159.2 352 152 359.2 152 368C152 376.8 159.2 384 168 384H193.6C219.2 384 240 363.2 240 337.6C240 320.1 231.1 305.6 216.6 297.4L191.2 282.9C186.8 280.3 184 275.5 184 270.4C184 262.4 190.4 256 198.4 256H216C224.8 256 232 248.8 232 240C232 231.2 224.8 224 216 224H198.4C172.8 224 152 244.8 152 270.4C152 287 160.9 302.4 175.4 310.6zM280 240C280 231.2 272.8 224 264 224C255.2 224 248 231.2 248 240V271.6C248 306.3 258.3 340.3 277.6 369.2L282.7 376.9C285.7 381.3 290.6 384 296 384C301.4 384 306.3 381.3 309.3 376.9L314.4 369.2C333.7 340.3 344 306.3 344 271.6V240C344 231.2 336.8 224 328 224C319.2 224 312 231.2 312 240V271.6C312 294.6 306.5 317.2 296 337.5C285.5 317.2 280 294.6 280 271.6V240zM256 0L384 128H256V0z"
                     },
-                    click: function (gd) {
+                    click: function(gd) {
                         console.log(gd);
                         tools.downloadBlob(maddog.tdcCSV, 'export.csv', 'text/csv;charset=utf-8;')
                     }
                 }]
-                
+
             });
-            
+
         },
         setTdcFeatures: (features) => {
             const tdcGeojson = `<![CDATA[{"type":"FeatureCollection","features":[${JSON.stringify(features)}]}]]>`;
@@ -244,11 +255,11 @@ const tdcUtils = (function() {
             });
             $("#drawRadialBtn").prop('disabled', features.length < 2);
         },
-        onDatesChange: () => {
+        changeTdc: () => {
             let selected = [];
             // clean graph
             if (document.getElementById("tdcChart")) {
-                tdcChart.remove();    
+                tdcChart.remove();
             }
             // get checked TDC
             $('#tdcMultiselect option:selected').each((i, el) => {
@@ -256,21 +267,37 @@ const tdcUtils = (function() {
             });
             // create coastline tracking param
             tdcUtils.setTdcFeatures(selected);
-            tdcUtils.drawTDC({ ...maddog.charts.tdc, features: selected });
+            tdcUtils.drawTDC({
+                ...maddog.charts.tdc,
+                features: selected
+            });
             if (maddog.charts.coastLines && maddog.charts.coastLines.result.length) {
                 let csv = _.flatten(maddog.charts.coastLines.result.filter(c => c.data.length).map(x => x.data));
-                maddog.tdcCSV = Papa.unparse(csv);   
+                maddog.tdcCSV = Papa.unparse(csv);
             }
 
-            // manage WPS trigger button
-            $("#coastlinetrackingBtn").prop("disabled", $('#tdcMultiselect option:selected').length < 2);
+            // set legend content
+            const legendHtml = selected.map(s => {
+                let color = "color:" + s.properties.color;
+                return `<li>
+                    <a class="labelDateLine">
+                        <label style="display:inline;padding-right: 5px;">${moment(s.properties.creationdate).format("DD/MM/YYYY")}</label>
+                        <i class="fas fa-minus" style='${color}'></i>
+                    </a>
+                </li>`
+            }).join("");
+            tdcUtils.changeLegend($(`<p>Date(s) sélectionnée(s):</p><ul class="nobullet">${legendHtml}</ul>`));
+        },
+        changeLegend: (content) => {
+            panelDrag?.display();
+            panelDrag?.clean();
+            panelDrag?.change(content);
         },
         manageError: () => {
             const displayError = $('#tdcMultiselect option:selected').length < 2;
             // manage trigger wps button
             $("#coastlinetrackingBtn").prop("disabled", displayError);
             panelTDCParam.hidden = displayError;
-            tdcResetBtn.hidden = displayError;
             alertTdcParams.hidden = !displayError;
         },
         createTDCMultiSelect: () => {
@@ -299,13 +326,16 @@ const tdcUtils = (function() {
                         </li>`
                 },
                 onChange: () => {
-                    tdcUtils.onDatesChange();
+                    tdcUtils.changeTdc();
                     tdcUtils.manageError();
                 },
             });
             // create options with multiselect dataprovider
-            let datesOptions = dates.map((d, i) => 
-                ({label: moment(d).format("DD/MM/YYYY"), value: d})
+            let datesOptions = dates.map((d, i) =>
+                ({
+                    label: moment(d).format("DD/MM/YYYY"),
+                    value: d
+                })
             );
             // insert options into multiselect
             $("#tdcMultiselect").multiselect('dataprovider', datesOptions);
@@ -319,7 +349,7 @@ const tdcUtils = (function() {
         },
         tdcReset: (cleanTdcLayer) => {
             if (document.getElementById("tdcChart")) {
-                tdcChart.remove();    
+                tdcChart.remove();
             }
             $("#tdcMultiselect").multiselect("refresh");
             $('.tdcNavTabs a[href="#tdcTabDate"]').tab('show');
@@ -328,15 +358,17 @@ const tdcUtils = (function() {
             mviewer.getLayer("radiales").layer.getSource().clear();
             if (!cleanTdcLayer) {
                 tdcUtils.getTDCByIdSite(maddog.idsite);
+                panelDrag.clean();
+                panelDrag.hidden();
             }
         },
         initTDC: () => {
             tdcUtils.tdcReset();
         },
         onParamChange: (e) => {
-            console.log(e);
-            maddog.setDrawRadialConfig({ [e.id]: e.type === "number" ? parseInt(e.value) : e.value });
-            console.log(maddog.drawRadialConfig[e.id]);
+            maddog.setDrawRadialConfig({
+                [e.id]: e.type === "number" ? parseInt(e.value) : e.value
+            });
         }
     }
 })();
--- mviewer/maddog/addons/panelDrag/panelDrag.js
+++ mviewer/maddog/addons/panelDrag/panelDrag.js
@@ -1,18 +1,18 @@
-
-const panelDrag = (function () {
-    
-    return {
-        
-        init: function () {
-          $('#panelDrag .panel').easyDrag(
-            {'container': $('#map'),
-            'handle': 'h3'
-            }
-          );
-            
-        }
-    };
-
+const panelDrag = (function() {
+  return {
+    init: function() {
+      $('#panelDrag .panel').easyDrag({
+        'container': $('#map'),
+        'handle': 'h3'
+      });
+    },
+    change: (html) => {
+      $('#panelDrag .panel-body').append(html);
+    },
+    display: () => $('#panelDrag').prop("hidden", false),
+    hidden: () => $('#panelDrag').prop("hidden", true),
+    clean: () => $('#panelDrag .panel-body').empty()
+  };
 })();
 
 new CustomComponent("panelDrag", panelDrag.init);

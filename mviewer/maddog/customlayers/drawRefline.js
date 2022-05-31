b74d10b5be6ca9d2bfcd95486b14ea52b11f49b6
gaetanbrl <gaetan.bruel@jdev.fr>
2022-05-31 17:34:56 +0200

    use multi style with TDC draw line ref

--- mviewer/maddog/customlayers/drawRefline.js
+++ mviewer/maddog/customlayers/drawRefline.js
@@ -1,10 +1,29 @@
 
-let styleLine = [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-        color: '#00d2ff',
-        width: 4
-    })
-})];
+let styleLine = [
+    new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: '#00d2ff',
+            width: 4
+        })
+    }),
+    new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: 5,
+            fill: new ol.style.Fill({
+                color: '#00d2ff',
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'white',
+                width: 2
+            })
+        }),
+        geometry: function (feature) {
+          // return the coordinates of the first ring of the line
+          const coordinates = feature.getGeometry().getCoordinates();
+          return new ol.geom.MultiPoint(coordinates);
+        },
+      })
+];
 
 let layer = new ol.layer.Vector({
     source: new ol.source.Vector(),

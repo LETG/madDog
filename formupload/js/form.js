2d30eee0568c291dfd6992d31805eba96d41676d
Agath21 <agathe.adam@jdev.fr>
2022-06-09 16:34:51 +0200

    Alert bootstrap #57

--- formupload/js/form.js
+++ formupload/js/form.js
@@ -79,14 +79,27 @@ function showDivProfil(elem){
     document.getElementById("formSuivi").reset();
 } 
 
-// Test du format du fichier uploader
+// Test du format du fichier uploader + Alerte
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.append(wrapper)
+}
+
 function validationFormat() { 
     var fichier = document.getElementById('formFile'); 
     var valeur = fichier.value; 
     var extensions = /(\.csv)$/i; 
         if (!extensions.exec(valeur))
         { 
-            alert('Format de fichier non valide, sélectionner un fichier .csv'); 
+            alert('Format de fichier non valide, veuillez sélectionner un fichier .csv','danger'); 
             fichier.value = ''; 
             return false; 
         } 

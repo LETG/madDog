cd5f5fa6e0723462a9d10b40dcc601c905e0cef6
Pierre Jego <pierre.jego@jdev.fr>
2022-06-27 23:20:18 +0200

    update imagemosaic integration

--- vrt/config-sample.sh
+++ vrt/config-sample.sh
@@ -8,3 +8,14 @@ maddogDBName=maddog
 maddogDBSchema=public
 maddogDBUser=maddog
 maddogDBPassword=maddog
+
+# gdal_grid
+gdalGridIndivParm="power=6.0:smothing=7.0:radius1=30.0:radius2=30.0:angle=90.0:max_points=0:min_points=1:nodata=-100.0"
+
+# geoserver dir for image mosaic
+mntDirectory=/srv/data/maddog/mnt
+
+# geoserver dir for image mosaic
+geoserverLogin=admin 
+geoserverMdp=admin
+geoserverUrl=http://localhost:8080/geoserver/rest/workspaces/maddog/coveragestores/mnt/coverages/file.imagemosaic
--- vrt/vrt2Postgis.sh
+++ vrt/vrt2Postgis.sh
@@ -3,7 +3,7 @@
 # lecture du fichier de configuration des connexions
 . config.sh
 
-echo ">START PROCESS"
+echo "--START PROCESS"
 idType="${1^^}" # MNT1 | PRF1 | TDC1 | REF1 | META
 fileName="$2" # csv or meta file with data
 fileNameWithoutExt="${fileName%.*}"
@@ -26,14 +26,14 @@ fi
 # choose VRT for defined type
 type=${idType:0:3}
 
-echo ">Type -> $type"
+echo "-- Type : $type and CodeSite : $codeSite"
 vrtFile="$type/$type.vrt"
 
 # update SrcDataSource
 # create temporary vrt file for this layer source
 configuredVrt="current.vrt"
 cp -pr $vrtFile $configuredVrt
-echo "Using file : $fileName "
+echo "-- Using file : $fileName "
 
 escapedFileName=$(echo $fileName | sed 's_/_\\/_g')
 
@@ -45,13 +45,35 @@ table="TMP$type"
 if [ $type = "MNT" ]
 then
     table="MNT"
+    #Add date to filename
+    mntDate=$(echo $fileName | cut -d "." -f1 | cut -d "_" -f3 )
+    mntGeoJson="TMP_${codeSite}_$mntDate.json"
+    mntOutputTmp="TMP_${codeSite}_$mntDate.tiff"
+    mntOutput="$mntDirectory/${codeSite}_$mntDate.tiff"
+
+    echo "mnt filename : $mntOutput"
+    
+    ogr2ogr -f GeoJSON "$mntGeoJson" "$configuredVrt" -where "\"identifiant\" NOT LIKE 'pc%'"
+    gdal_grid -zfield z -a invdist:$gdalGridIndivParm -ot Float64 -of GTiff "$mntGeoJson" "$mntOutputTmp"
+    echo "-- Update value for file $mntOutput"
+    gdalwarp $mntOutputTmp  $mntOutput
+
+    chown -R tomcat:tomcat $mntOutput
+   
+    #update mosaic index
+    echo curl -v -u $geoserverLogin:$geoserverMdp -XPOST $geoserverUrl -H "\"Content-type: text/plain\"" -d "\"file://$mntDirectory\""
+    curl -v -u $geoserverLogin:$geoserverMdp -XPOST $geoserverUrl -H "\"Content-type: text/plain\"" -d "\"file://$mntDirectory\"" 
+
+    rm $mntGeoJson
+    rm $mntOutputTmp
+else 
+    echo "-- Import file to postgresql in table : $table"
+    ogr2ogr -append -f "PostgreSQL" PG:"host=$maddogDBHost user=$maddogDBUser port=$maddogDBPort dbname=$maddogDBName password=$maddogDBPassword schemas=$maddogDBSchema" -nln "$table" $configuredVrt
 fi
-echo ">Import file to postgresql in table : $table"
-ogr2ogr -append -f "PostgreSQL" PG:"host=$maddogDBHost user=$maddogDBUser port=$maddogDBPort dbname=$maddogDBName password=$maddogDBPassword schemas=$maddogDBSchema" -nln "$table" $configuredVrt
 
 if [[ $type == "REF" ]]
 then
-    echo ">Create line for point in LineREF"
+    echo "-- Create line for point in LineREF"
     ## Need to add date
     PGPASSWORD=$maddogDBPassword psql -h $maddogDBHost -p $maddogDBPort -d $maddogDBName -U $maddogDBUser -c "INSERT INTO LINEREF(idSite, idType, geom) SELECT '$codeSite', idType, line FROM (SELECT idType, ST_Makeline(wkb_geometry ORDER BY linePosition) as line FROM $table GROUP BY idType) as allLines;"
     #DROP TempTable
@@ -61,14 +83,14 @@ then
     PGPASSWORD=$maddogDBPassword psql -h $maddogDBHost -p $maddogDBPort -d $maddogDBName -U $maddogDBUser -c "REFRESH MATERIALIZED VIEW communewithsite;"
 elif [[ $type == "TDC" ]]
 then
-    echo ">Create line for point in Import TDC"
+    echo "-- Create line for point in Import TDC"
     ## Need to add date and TDC number
     PGPASSWORD=$maddogDBPassword psql -h $maddogDBHost -p $maddogDBPort -d $maddogDBName -U $maddogDBUser -c "INSERT INTO TDC(idSite, idType, creationDate, geom) SELECT '$codeSite', '$idType', creationDate, ST_Makeline(ARRAY(SELECT wkb_geometry FROM $table ORDER BY lineposition)) FROM (SELECT creationDate FROM $table WHERE creationDate IS NOT NULL LIMIT 1) as firstDate;"
     #DROP TempTable
     PGPASSWORD=$maddogDBPassword psql -h $maddogDBHost -p $maddogDBPort -d $maddogDBName -U $maddogDBUser -c "DROP TABLE $table;"
 elif [[ $type == "PRF" ]]
 then
-    echo ">Create line for point in Import PRF"
+    echo "-- Create line for point in Import PRF"
     ## Need to add date and PRF number
     PGPASSWORD=$maddogDBPassword psql -h $maddogDBHost -p $maddogDBPort -d $maddogDBName -U $maddogDBUser -c "INSERT INTO PRF(idSite, idType, creationDate, geom) SELECT '$codeSite', '$idType', creationDate, ST_Makeline(ARRAY(SELECT wkb_geometry FROM $table ORDER BY lineposition)) FROM (SELECT creationDate FROM $table WHERE creationDate IS NOT NULL LIMIT 1) as firstDate;"
     #DROP TempTable
@@ -76,7 +98,7 @@ then
 fi
 
 # clean temporary VRT file
-echo ">Cleanning temporary file"
+echo "-- Cleanning temporary file"
 rm $configuredVrt
 
-echo ">IMPORT SUCCESS"
+echo "-- IMPORT SUCCESS"

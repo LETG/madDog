6b6c24e5b7e4c038d2cb10b216c1ba0a25956730
Agath21 <agathe.adam@jdev.fr>
2022-06-28 15:51:31 +0200

    Get select value

--- formupload/front/js/wps-form.js
+++ formupload/front/js/wps-form.js
@@ -2,6 +2,13 @@
 // Create variable to content csv
 var csvContent = null;
 
+// Function to get select's value
+function getValSelect(selectId){
+    var value = document.getElementById(selectId).value;
+    return value;
+}
+
+// Function ton import Maddog Data with wps
 function importDataWPS() {
     ExecuteResponse_v1_xml = ExecuteResponse_v1_xml.extend({
         instantiate: (wpsResponse) => {
@@ -16,13 +23,13 @@ function importDataWPS() {
     let inputGenerator = new InputGenerator();
     // input
     const inputs = Object.values({
-        measureType: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("measureType", null, null, measureType.values),
-        codeSite: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("codeSite", null, null, codeSite),
-        numProfil: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("numProfil", null, null, numProfil),
-        surveyDate: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("surveyDate", null, null, surveyDate),
-        epsg: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("epsg", null, null, epsg),
-        idEquipement: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("idEquipement", null, null, idEquipement),
-        idOperator: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("idOperator", null, null, idOperator),
+        measureType: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("measureType", null, null, getValSelect("measureType")),
+        codeSite: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("codeSite", null, null, getValSelect("codeSite")),
+        numProfil: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("numProfil", null, null, getValSelect("numProfil")),
+        surveyDate: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("surveyDate", null, null, getValSelect("surveyDate")),
+        epsg: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("epsg", null, null, getValSelect("epsg")),
+        idEquipement: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("idEquipement", null, null, getValSelect("idEquipement")),
+        idOperator: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("idOperator", null, null, getValSelect("idOperator")),
         csvContent: inputGenerator.createLiteralDataInput_wps_1_0_and_2_0("csvContent", null, null, csvContent)
     });
     // output

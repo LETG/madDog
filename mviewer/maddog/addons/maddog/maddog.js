f3d7a8e2f17a376e7f00e423248a86efb33b621b
gaetanbrl <gaetan.bruel@jdev.fr>
2022-08-18 12:18:26 +0200

    #68 change zoom on view init from URL

--- mviewer/maddog/addons/maddog/maddog.js
+++ mviewer/maddog/addons/maddog/maddog.js
@@ -16,8 +16,10 @@ const maddog = (function() {
     document.addEventListener("communes-ready", () => {
         let { x, y, z } = API;
         if (!x || !y || !z) {
-            tools.zoomToOGCLayerExtent();   
+            return tools.zoomToOGCLayerExtent();   
         }
+        const thisView = mviewer.getMap().getView();
+        return thisView.setZoom(thisView.getZoom() - 1);
     });
 
     // wait many lib to avoid race condition errors

07dd28bc29a09e342fa67370197123715c437e48
gaetanbrl <gaetan.bruel@jdev.fr>
2023-12-19 17:54:14 +0100

    Fix #119 if legend is under nav

--- mviewer/maddog/addons/panelDrag/panelDrag.js
+++ mviewer/maddog/addons/panelDrag/panelDrag.js
@@ -2,8 +2,17 @@ const panelDrag = (function() {
   return {
     init: function() {
       $('#panelDrag .panel').easyDrag({
-        'container': $('#map'),
-        'handle': 'h3'
+        container: $('#map'),
+        handle: 'h3',
+        stop: (e) => {
+          const navBarClientInfos = document.querySelector("#mv-navbar").getBoundingClientRect();
+          const panelDragClientInfos = document.querySelector("#panelDrag .panel").getBoundingClientRect();
+          let minYNavBar = navBarClientInfos.height + navBarClientInfos.top;
+          let isUnderNavbar = panelDragClientInfos.top < minYNavBar;
+          if (isUnderNavbar) {
+            document.querySelector("#panelDrag .panel").style.top = "5px";
+          }
+        }
       });
     },
     change: (html) => {

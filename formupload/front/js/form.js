2b5c34d33b1b6229d8953a94c639c8d6706567ed
Pierre Jégo <pierre.jego@jdev.fr>
2025-03-04 17:41:10 +0100

    link to #43
    Add validation on front side for Maddog import

--- formupload/front/js/form.js
+++ formupload/front/js/form.js
@@ -30,12 +30,12 @@ const config = [{
 ]
 
 // Function to filter Json by value
-function find_in_object(object, my_criteria){
-    return object.filter(function(obj) {
-      return Object.keys(my_criteria).every(function(c) {
-        return obj[c] == my_criteria[c];
-      });
-    });  
+function find_in_object(object, my_criteria) {
+    return object.filter(function (obj) {
+        return Object.keys(my_criteria).every(function (c) {
+            return obj[c] == my_criteria[c];
+        });
+    });
 };
 
 
@@ -50,19 +50,19 @@ function optionsGenerator(value, text, select) {
 
 // Function to add all options in select by API
 function addOptionsSelect(url, field, textfield, idField, idSelect) {
-fetch(url)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(json) {
-        json.forEach(function(feature, index) {
-            // If field as null display id field            
-            let fieldSelect = feature[field] ? feature[field] : feature[idField];
-            let fieldText = feature[textfield] ? feature[textfield] : fieldSelect;
-            // Create select's option for all feature
-            optionsGenerator(fieldSelect, fieldText, idSelect)
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (json) {
+            json.forEach(function (feature, index) {
+                // If field as null display id field            
+                let fieldSelect = feature[field] ? feature[field] : feature[idField];
+                let fieldText = feature[textfield] ? feature[textfield] : fieldSelect;
+                // Create select's option for all feature
+                optionsGenerator(fieldSelect, fieldText, idSelect)
+            });
         });
-    });
 };
 
 // Add all options for config object
@@ -74,10 +74,10 @@ const selectMeasure = document.querySelector('#measureType');
 selectMeasure.addEventListener('change', (event) => {
     var value = event.target.value;
     // IF PRF display numProfil information
-    if (value == 'PRF') {        
+    if (value == 'PRF') {
         // Integrate the profile numbers according to the selected site
         const selectSite = document.querySelector('#codeSite');
-        selectSite.addEventListener('change', f = (event) => { 
+        selectSite.addEventListener('change', f = (event) => {
             // Display PRF select
             document.getElementById('selectProfilId').style.display = "block";
             // Remove options from select
@@ -89,24 +89,24 @@ selectMeasure.addEventListener('change', (event) => {
                 let responseSite = await fetch(url + "/maddogapi/site");
                 let jsonSite = await responseSite.json();
                 // Filter site by code site
-                var filtered_jsonSite = find_in_object(JSON.parse(JSON.stringify(jsonSite)), {code_site: codeSite});
+                var filtered_jsonSite = find_in_object(JSON.parse(JSON.stringify(jsonSite)), { code_site: codeSite });
                 var idSite = filtered_jsonSite[0].id_site;
                 // Get Profils for selected site
                 let responsePRF = await fetch(url + "/maddogapi/sitemeasureprofil");
                 let jsonPRF = await responsePRF.json();
                 // Filter profiles by site id
-                var filtered_jsonPRF = find_in_object(JSON.parse(JSON.stringify(jsonPRF)), {id_site: idSite});
+                var filtered_jsonPRF = find_in_object(JSON.parse(JSON.stringify(jsonPRF)), { id_site: idSite });
                 // Filter profiles by the type of measurement
-                var PRF = find_in_object(filtered_jsonPRF, {measuretype: 'PRF'});
+                var PRF = find_in_object(filtered_jsonPRF, { measuretype: 'PRF' });
                 // Sort profiles on num_profil
                 PRF.sort((a, b) => a.num_profil - b.num_profil);
                 // Display option if not profil
-                if (!$.isArray(PRF) ||  !PRF.length){                    
+                if (!$.isArray(PRF) || !PRF.length) {
                     optionsGenerator('', "Aucun profil n'est disponible pour le site sélectionné", 'numProfil');
                 } else {
                     PRF.forEach(el => optionsGenerator(el.num_profil, "Profil " + el.num_profil, 'numProfil'));
-                }                
-            }             
+                }
+            }
             fetchUrl();
         });
     } else {
@@ -136,21 +136,29 @@ function validationFormat() {
         csvFile.setCustomValidity("Format de fichier non valide, veuillez sélectionner un fichier .csv");
         csvFile.reportValidity();
         csvFile.value = '';
-        return ;
-    } else {   
+    } else {
         // Read csv file to string
         var files = csvFile.files;
         if (files.length === 0) {
             csvFile.setCustomValidity("Le fichier ne doit pas être vide");
             csvFile.reportValidity();
-            return ;
+        } else {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                const csvContent = event.target.result;
+                const result = validateCSV(csvContent, selectMeasure.value);
+                if (!result.valid) {
+                    csvFile.setCustomValidity(result.error);
+                    csvFile.reportValidity();
+                    console.error('Erreur:', result.error);
+                } else {
+                    document.getElementById('maddogImpBut').disabled = false;
+                    csvFile.setCustomValidity("");
+                    console.error('Valide:');
+                }
+            };
+            reader.readAsText(files[0]);
         }
-        csvFile.setCustomValidity("");
-        var reader = new FileReader();
-        reader.onload = function(event) {
-            csvContent = event.target.result;
-        };
-        reader.readAsText(files[0]);
     }
 }
 
@@ -160,12 +168,12 @@ function validationFormat() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const formSuivi = document.getElementById("formSuivi");
 
-    formSuivi.addEventListener('submit', event => { 
+    formSuivi.addEventListener('submit', event => {
 
         event.preventDefault();
-        if (!formSuivi.checkValidity()) {           
+        if (!formSuivi.checkValidity()) {
             event.stopPropagation();
-        }else{
+        } else {
             importDataWPS()
         }
         formSuivi.classList.add('was-validated')
---
+++ formupload/front/js/validate-data.js
@@ -0,0 +1,96 @@
+function validateCSV(content, measureType) {
+    const lines = content.trim().split('\n');
+    const headers = lines[0].replace(/\r/g, '').split(';');
+
+    console.log(headers);
+    // Vérification des noms des entêtes
+    const expectedHeaders = ['id', 'x', 'y', 'z', 'identifiant', 'date'];
+    if (!arraysEqual(headers, expectedHeaders)) {
+        return { valid: false, error: 'En-têtes incorrects ou dans le mauvais ordre.' };
+    }
+
+    // Vérification de l'ordre des id (ordre croissant)
+    // Vérification du type de séparateur décimal pour les coordonnées (.)
+    // Vérification du type de séparateur de champs (;)
+    // Vérification des identifiants
+    // Vérification du format de date (aaaa-mm-jj)
+    const identifiants = [];
+    let previousId = -1;
+    //let previousIdentifiant = null;
+    //let previousDate = null;
+
+    for (let i = 1; i < lines.length; i++) {
+        const columns = lines[i].replace(/\r/g, '').split(';');
+
+        if (columns.length !== expectedHeaders.length) {
+            return { valid: false, error: `Nombre incorrect de colonnes à la ligne ${i + 1}.` };
+        }
+
+        const id = parseInt(columns[0], 10);
+        if (isNaN(id) || id <= previousId) {
+            return { valid: false, error: `ID invalide ou non croissant à la ligne ${i + 1}.` };
+        }
+        previousId = id;
+
+        const x = parseFloat(columns[1]);
+        const y = parseFloat(columns[2]);
+        const z = columns[3].trim();
+        if (isNaN(x) || isNaN(y) || (z !== '' && !isValidDecimal(z))) {
+            return { valid: false, error: `Coordonnées invalides à la ligne ${i + 1}.` };
+        }
+
+        const identifiant = columns[4];
+        if (!isValidIdentifiant(identifiant)) {
+            return { valid: false, error: `Identifiant invalide à la ligne ${i + 1}.` };
+        }
+        identifiants.push(identifiant);
+        // Peut être pas nécessaire
+        // if (previousIdentifiant && identifiant !== previousIdentifiant) {
+        //     return { valid: false, error: `Identifiant différent à la ligne ${i + 1}.` };
+        // }
+        // previousIdentifiant = identifiant;
+
+        const date = columns[5];
+        // on vérifie les dates uniquement si ce n'est pas une mesure de type ref
+        if (!isValidDate(date) && measureType != "REF") {
+            return { valid: false, error: `Format de date invalide à la ligne ${i + 1}.` };
+        }
+        // // Problèment pas nécessaire
+        // if (previousDate && date !== previousDate) {
+        //     return { valid: false, error: `Date différente à la ligne ${i + 1}.` };
+        // }
+        //previousDate = date;
+    }
+    if (!identifiants.includes(measureType + "1")) {
+        return { valid: false, error: `Le csv doit contenir au moins un identifiant de la forme ${measureType}1` };
+    }
+
+    return { valid: true, error: null };
+}
+
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+function isValidIdentifiant(identifiant) {
+    const prfMatch = identifiant.match(/^PRF\d+$/);
+    const tdcMatch = identifiant.match(/^TDC\d+$/);
+    const startWithL = identifiant.match(/^l\d+$/);
+    const startWithpc = identifiant.match(/^pc\d+$/);
+    return prfMatch || tdcMatch || startWithL || startWithpc;
+}
+
+function isValidDate(date) {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    return dateRegex.test(date);
+}
+
+function isValidDecimal(value) {
+    const decimalRegex = /^-?\d+(\.\d+)?$/;
+    return decimalRegex.test(value);
+}
+
